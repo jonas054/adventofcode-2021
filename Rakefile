1e06efbcd58c2df32301c31cfa83e3c2ae8eabf5
Jonas Arvidsson <jonas054@gmail.com>
2021-12-04 16:27:56 +0100

    Copy debugging file from 2020 repo
    Better have a copy here, so we don't depend on the 2020 repo being present.

--- Rakefile
+++ Rakefile
@@ -1,5 +1,5 @@
 task :default do
   newest = Dir['[0-9b]*.rb'].max_by { File.new(_1).mtime }
   sh "rubocop -A #{newest}"
-  ruby "-I../2020 -rrainbow -rdebugging #{newest}"
+  ruby "-I. -rrainbow -rdebugging #{newest}"
 end
---
+++ debugging.rb
@@ -0,0 +1,25 @@
+def output_with_color(color, *args)
+  args.each do |arg|
+    puts '  ' * (caller.length - 4) + Rainbow(arg).send(color)
+  end
+end
+
+def debug(*args)
+  output_with_color :yellow, *args
+end
+
+def info(*args)
+  output_with_color :cyan, *args
+end
+
+def warning(*args)
+  output_with_color :pink, *args
+end
+
+def error(*args)
+  output_with_color :red, *args
+end
+
+def green(*args)
+  output_with_color :green, *args
+end
